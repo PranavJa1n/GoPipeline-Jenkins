55fc56b8c6dbd65650b7843a030beb096753b5a1
Pranav Jain <pranav.avlok@gmail.com>
2025-06-13 16:00:15 +0530

    Update main_test.go

--- main_test.go
+++ main_test.go
@@ -7,15 +7,15 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	a := 0
-	for a != 2 {
-		rand.Seed(time.Now().UnixNano())
-		num := rand.Intn(10) + 1
-		result := hello(num)
-		if result != "Hello" {
-			t.Error("Expected Hello but got emply return")
-			return
-		}
-		a += 1
+	// a := 0
+	// for a != 2 {
+		// rand.Seed(time.Now().UnixNano())
+		// num := rand.Intn(10) + 1
+	result := hello(5)
+	if result != "Hello" {
+		t.Error("Expected Hello but got emply return")
+		return
 	}
+		// a += 1
+	// }
 }
